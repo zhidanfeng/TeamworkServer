fdd894c583b504e526f8a61327de39496eca715e
zhidanfeng <1083607697@qq.com>
2020-08-06 20:22:26 +0800

    复写异常的fillInStackTrace，提升性能

--- teamwork-common/src/main/java/com/teamwork/exceptions/TeamworkException.java
+++ teamwork-common/src/main/java/com/teamwork/exceptions/TeamworkException.java
@@ -22,4 +22,16 @@ public class TeamworkException extends RuntimeException {
         super(msg);
         this.errorCode = ResultCode.ILLEGAL_ARGUMENT_EXCEPTION.code();
     }
+
+    /***
+     * @name: fillInStackTrace
+     * @description: 避免对api异常进行昂贵且无用的堆栈跟踪
+     * @return: java.lang.Throwable
+     * @since: 2020/8/6 20:16
+     * @author: zhidanfeng
+     */
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        return this;
+    }
 }
